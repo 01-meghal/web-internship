fa9bd4590c7c557be873b1a3cf0aaa767d7980cf
meghal limba <limbameghal8@gmail.com>
2025-05-21 10:33:12 +0530

    updated

---
+++ day11/datatype.js
@@ -0,0 +1,32 @@
+let a =20;
+console.log(a)
+console.log(typeof a)
+
+let b="44587";
+console.log(b)
+console.log(typeof b)
+
+let c=true;
+console.log(c)
+console.log(typeof c)
+
+let d=null;
+console.log(d)
+console.log(typeof d)
+
+let e;
+console.log(e)
+console.log(typeof e)
+
+let id1=Symbol("key1");
+let id2=Symbol("key2");
+console.log(id1==id2)
+
+let user ={
+    name:"meghal",
+    age:20,
+    city:"jodhpur"
+
+}
+console.log(user)
+console.log(typeof user)
