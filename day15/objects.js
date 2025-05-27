3080a73654748cc3f28650754449a6c3717feb1b
meghal limba <limbameghal8@gmail.com>
2025-05-27 10:51:34 +0530

    updated

---
+++ day15/objects.js
@@ -0,0 +1,10 @@
+let one =Symbol("key1")
+let user ={
+    "username ": "janak",
+    age :23,
+    [one] : "key2",
+    ismarried : false,
+    lastlength :["tuesday","saturday"]
+}
+//console.log(user["username "])
+console.log(user)
---
+++ day15/strings.js
@@ -0,0 +1,31 @@
+// let a="hello";
+// let b="world";
+// let c=b.concat(a);
+// console.log(c)
+
+//---------------------------
+// let a="my name is lakhan";
+// let b=a.startsWith("m");
+// console.log(b)
+
+//------------------------------
+// let a="tarzan";
+// let b= a.endsWith("n");
+// console.log(b)
+
+//----------------------------------
+// let a="javascript";
+// let b= a.length
+// console.log(b)
+
+//---------------------------------
+// let a ="jaishankar";
+// let b =a.indexOf("i");
+// console.log(b)
+
+//--------------------------------
+// let a ="jaishankar";
+// let b =a.lastIndexOf("k");
+// console.log(b)
+
+//----------------------------------
